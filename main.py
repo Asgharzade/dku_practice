986e08e0479075a2653c8061b81465293df50670
Asgharzade <amin.asgharzade@gmail.com>
2025-01-23 23:27:51 -0600

    added the light gbm and rfc, gitignore, main.py

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-from ML.rfc_v3 import rfc
+from ML.rfc_v1 import classifier as classifier_rfc
+from ML.lgbn_v3 import classifier as classifier_lgbn
+from preprocessing import preprocess
 
-# run ML process
-rfc()
+def main():
+
+    '''
+    this is the main function that performs
+    1. pre-processing etl
+    2. run ML classifier for light gbm
+    3. run ML classifier for random forest
+    '''
+
+    #run pre-processing
+    preprocess()
+
+    # #run ML process for the Random Forest Classifier
+    # classifier_rfc()
+
+    # #run ML process for the Light GBM Classifier
+    classifier_lgbn()
+
+if __name__ == "__main__":
+    main()
